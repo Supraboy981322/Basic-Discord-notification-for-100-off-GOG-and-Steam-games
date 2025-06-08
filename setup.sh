1ad7d2a0ee7327f708dfe77bd3b6352a5760dd6f
Supraboy981322 <97927259+Supraboy981322@users.noreply.github.com>
2025-06-08 16:34:19 -0500

    FIXED IT

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ echo "The package will be placed in your /home/$USER directory"
 # download package
 mkdir free-games-checker
 cd free-games-checker
-wget "https://github.com/Supraboy981322/Basic-Discord-notification-for-100-off-GOG-and-Steam-games/raw/refs/heads/main/free-games-checker.zip" > /dev/null
+wget "https://github.com/Supraboy981322/Basic-Discord-notification-for-100-off-GOG-and-Steam-games/raw/refs/heads/main/free-games-checker.zip" &> /dev/null
 unzip free-games-checker.zip > /dev/null
-sudo rm free-games-checker.zip
+rm free-games-checker.zip
 
 # ask user for discord api webhook url
 echo "In order to communicate with Discord, this script needs a Discord API webhook URL, please create one if not already done."
@@ -18,15 +18,15 @@ read DiscordURL
 echo "Setting the value of 'discord_url' in 'send-gog.sh' and 'send-steam.sh'"
 # set the value of "discord_url" in send-gog.sh and send-steam.sh
 DEFAULT_DISCORD_URL="YOUR_DISCORD_WEBHOOK_URL_GOES_HERE"
-sed -i "s/${DEFAULT_DISCORD_URL}/${DiscordURL}/g" "${send-steam.sh}"
-sed -i "s/${DEFAULT_DISCORD_URL}/${DiscordURL}/g" "${send-gog.sh}"
+sed -i "s|${DEFAULT_DISCORD_URL}|${DiscordURL}|g" send-steam.sh
+sed -i "s|${DEFAULT_DISCORD_URL}|${DiscordURL}|g" send-gog.sh
 
 # ask user how frequently to check for sales and what time
 echo "The script needs to know how frequently to check for game sales"
 echo "Keep in mind that it will notify you if there is a 100% off game everytime it finds one, even if it has already notified you of it."
 echo "How often should it run?"
 echo "Daily (d), weekly (w), monthly (m)"
-read FREQUENCY
+read -p "daily (d), weekly (w), monthly (m): " FREQUENCY
 echo "At what hour of the day should it run?"
 echo "Use 24 hour format, ex: '23' (which is 11 PM)"
 read HOUR
@@ -36,25 +36,25 @@ read MINUTE
 
 # create cronjob
 
-if [$FREQUENCY = "d"]; then 
-   DAY=*
-   WEEK=*
-   MONTH=*
-else if [$FREQUENCY = "w"]; then
-   DAY=*
-   MONTH=*
-   WEEK=0
-else if [$FREQUENCY = "m"]; then
-   DAY=1
-   MONTH=*
-   WEEK=*
+if [[ "$FREQUENCY" == "d" ]]; then 
+   DAY="*"
+   WEEK="*"
+   MONTH="*"
+elif [[ "$FREQUENCY" == "w" ]]; then
+   DAY="*"
+   MONTH="*"
+   WEEK="0"
+elif [[ "$FREQUENCY" == "m" ]]; then
+   DAY="1"
+   MONTH="*"
+   WEEK="*"
 fi
 
 crontab -l > mycron
 #echo new cron into cron file
-echo "$MINUTE $HOUR $DAY $MONTH $WEEK ~/free-games-checker/./master.sh" >> mycron
+echo "$MINUTE $HOUR $DAY $MONTH $WEEK $HOME/free-games-checker/./master.sh" >> mycron
 #install new cron file
 crontab mycron
-sudo rm mycron
+rm mycron
 
 echo "Thank you for installing my Steam and GOG free game sale Discord bot."
