8bb97e9e8ae1ba5b4b428b985baf01a68836471e
Zane <97927259+Supraboy981322@users.noreply.github.com>
2025-10-24 17:10:24 -0500

    rewrite setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,85 @@
-printf "free-games-checker was originally written in Bash, but is currently being rewritten in Go.\n"
-printf "because of this, the install script wouldn't work correctly.\n"
-printf "if you insist on using the incomplete rewrite, download the source and compile it manually.\n"
-printf "if compiling it yourself is a problem, you're better off waiting.\n"
+#!/bin/bash
+set -e
+
+projectName="free-games-checker"
+projectAuthor="Supraboy981322"
+
+printf "Would you like to fetch binaries (fastest and safest), compile the code locally?\n"
+printf "b [binaries]  or  c [compile]\n"
+read binOrComp
+
+printf "checking for dependencies\n"
+for dep in "jq" "bash" "bzip2" "tar"; do
+    if [ -z $(command -v ${dep}) ]; then
+         missingDeps+=" - '${dep}'\n"
+    fi
+done
+
+if [[ ! -z "${missingDeps}" ]]; then
+    printf "The ${free-games-checker} install is missing the following dependencies:\n"
+    printf "${missingDeps}"
+    exit 1
+fi
+
+function setSettingsJSON() {
+    printf "${projectName} requires 3 Discord webhooks per store.\n"
+    printf "If you do not know how to get Discord webhooks, the internet is a great place to ask.\n"
+    printf "Continue?\n"
+    printf "[y/n]\n"
+    read cont
+    ls
+    if [[ "$cont" == "y" ]]; then
+        for store in "Steam" "GOG"; do
+            case "${store}" in
+                "Steam")
+                    storeNum=0
+                    ;;
+                "GOG")
+                    storeNum=1
+                    ;;
+                "Itch")
+                    storeNum=2
+                    ;;
+                "Epic")
+                    storeNum=3
+                    ;;
+                *)
+                    printf "ERR! INVALID STORE ENTERED!"
+                    printf "THIS LIKELY MEANS SOMEONE'S DOING SOMETHING NASTY!"
+                    exit 1
+                    ;;
+            esac
+            printf "Now, you will need webhooks to use for ${store}\n"
+            for num in $(seq 0 2); do
+                printf "Please enter webhook $((num+1)) for ${store}\n"
+                read webhook
+                jq ".[${storeNum}].webhooks.[${num}] = \"${webhook}\"" settings.json > tmp-settings.json
+                rm settings.json
+                mv tmp-settings.json settings.json
+            done
+        done
+        printf "settings complete"
+    else
+        printf "exiting...\n"
+        exit 0
+    fi
+}
+
+case "$binOrComp" in
+    "b")
+        printf "creating 'free-games-checker' directory\n"
+        mkdir free-games-checker
+        cd free-games-checker
+        printf "fetching '.bzip2' tarball\n"
+        wget "https://raw.githubusercontent.com/${projectAuthor}/${projectName}/main/build/package/free-games-checker.tar.bz2" &> /dev/null
+        printf "extracting '.bzip2 tarball\n"
+        tar -xjf free-games-checker.tar.bz2 &> /dev/null
+        printf "cleanup\n"
+        rm free-games-checker.tar.bz2
+        setSettingsJSON
+        ;;
+    *)
+        printf "unrecognized option, exiting\n"
+        ;;
+esac
+
